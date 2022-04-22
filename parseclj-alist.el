4d0e780e00f1828b00c43099e6eebc6582998f72
Arne Brasseur <arne@arnebrasseur.net>
2022-04-22 11:27:46 +0200

    More warning fixes

--- parseclj-alist.el
+++ parseclj-alist.el
@@ -27,6 +27,8 @@
 
 ;;; Code:
 
+(require 'map)
+
 (defun parseclj-alist (&rest kvs)
   "Create an association list from the given keys and values KVS.
 Arguments are simply provided in sequence, rather than as lists or cons cells.
@@ -82,7 +84,7 @@ For example: (parseclj-hash-table :foo 123 :bar 456)"
   ;; Emacs 27: (map-merge 'alist l1 l2)
   (let ((keys (delete-dups (append (mapcar #'car l1) (mapcar #'car l2))))
         (res '()))
-    (mapcar
+    (mapc
      (lambda (key)
        (push (or (assoc key l2)
                  (assoc key l1))
